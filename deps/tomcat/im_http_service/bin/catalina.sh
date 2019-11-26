b5ee8f920bd7879b9785fc3296fcdb39923eb9f0
zhangchao11 <zhangchao11@360.cn>
2019-11-26 13:21:25 +0800

    remove webapps with im_http_service

--- deps/tomcat/im_http_service/bin/catalina.sh
+++ deps/tomcat/im_http_service/bin/catalina.sh
@@ -115,8 +115,8 @@
 #                   case the default is "true"
 # -----------------------------------------------------------------------------
 
-CATALINA_HOME=/home/zhangchao/work/talk/openresty_ng/deps/tomcat/im_http_service
-CATALINA_BASE=/home/zhangchao/work/talk/openresty_ng/deps/tomcat/im_http_service
+CATALINA_HOME=/home/zhangchao11/work/talk/openresty_ng/deps/tomcat/im_http_service
+CATALINA_BASE=/home/zhangchao11/work/talk/openresty_ng/deps/tomcat/im_http_service
 
 # OS specific support.  $var _must_ be set to either true or false.
 cygwin=false
